3bb50536934bf278a28caa68f65c997d5ce2d803
Sergio Gonzalez <bigmonachus@gmail.com>
2012-09-24 19:56:30 -0500

    imgs, descr

--- pcs_main/pcs_main.go
+++ pcs_main/pcs_main.go
@@ -8,32 +8,15 @@ import (
 	"net/http"
 )
 
-const deploy = false;
-
-var main_template string
-
-type pcs_main_data struct {
-	BandDescr   string
-	DanielDescr string
-}
-
 func init() {
 	http.HandleFunc("/", handler)
 }
 	
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Hello")
-	d := pcs_main_data{
-		BandDescr: `
-The Pie Crust Shell is a Progressive Rock group that kicks serious ass.
-`,
-		DanielDescr: `
-Daniel Gonzalez is a cool dude and a fucking awesome musician.
-			      He currently lives in Boston and is a student at Berklee.
-`}
 	t, err := template.ParseFiles("tmpl/pcs_main.html")
 	if err != nil {
 		panic(err)
 	}
-	t.Execute(w, d)
+	t.Execute(w, nil)
 }
