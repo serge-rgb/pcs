57c1e37e7eb035fbc365bad4834c5e6629987442
Sergio Gonzalez <bigmonachus@gmail.com>
2014-01-16 03:22:49 -0600

    too much responsibility in JS file

--- dan.py
+++ dan.py
@@ -4,18 +4,24 @@ from fileinput import close
 from mimetypes import init
 
 USE_MIN = False
-# USE_MIN = True
+USE_MIN = True
 
+MIN_BLACKLIST = ['js/dan.js', 'css/dan.css']
 
 def static(name):
     'foo.ext -> (foo.ext | foo.min.ext)'
     prelude = 'static/'
-    if not USE_MIN:
-        return prelude + name
     strs = name.split('.')
     fname = strs[0]
     ext = strs[1]
-    return prelude + fname + '.min.' + ext
+
+    if not USE_MIN or ext == "png" or name in MIN_BLACKLIST:
+        return prelude + name
+
+    if USE_MIN and ext == 'js' or ext == 'css' and not (name in MIN_BLACKLIST):
+        return prelude + fname + '.min.' + ext
+
+    return "NOPE"
 
 
 app = Flask(__name__)
@@ -23,9 +29,11 @@ app.debug = True
 
 
 class Album(object):
-    def __init__(self, img, text):
+    def __init__(self, img, text, tracks):
         self.img = img
         self.text = text
+        self.tracks = tracks
+
 
 @app.route('/')
 def hello_world():
@@ -33,16 +41,20 @@ def hello_world():
     dan_src = dan_file.read()
     dan_file.close()
     tmpl = Template(dan_src)
+    albums = [Album(static('img/Placeholder.png'),
+                    'foo',
+                    ['https://soundcloud.com/dan_music/last-time'])]
     index_str = tmpl.render(name='Daniel',
                         bootstrap_js_fname=static('js/bootstrap.js'),
                         bootstrap_css_fname=static('css/bootstrap.css'),
                         dan_css_fname=static('css/dan.css'),
-                        albums=[Album(static('img/Placeholder.png'), 'foo') for _ in xrange(4)],
-                        album_1=static('img/Placeholder.png'))
+                        dan_js_fname=static('js/dan.js'),
+                        albums=albums,
+                        )
     return index_str
 
 
 if __name__ == '__main__':
 #     app.debug = False
-#     app.run(host='0.0.0.0')
-    app.run()
+    app.run(host='0.0.0.0')
+#     app.run()
