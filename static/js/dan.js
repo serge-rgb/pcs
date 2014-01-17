c69cdcc860cb32a3b6287ae5a1c76551e4e11bce
Sergio Gonzalez <bigmonachus@gmail.com>
2014-01-17 00:58:52 -0600

    Toggle album

--- static/js/dan.js
+++ static/js/dan.js
@@ -1,28 +1,49 @@
-function getIframe(url, element) {
-	console.log("getting iframe for: " + url);
-	SC.oEmbed(url, {
-		auto_play : false
-	}, function(oembed) {
-		console.log('embed return: ' + oembed.title)
-		console.log('embed return: ' + oembed.html)
-		element.html(oembed.html);
-	});
-}
+function scope() {
+	var chosen_album = "none";
 
-$(document).ready(function() {
-	$(".album-table").each(function() {
-		$(this).find(".track").each(function() {
-			getIframe($(this).find(".track-url").html(),
-					$(this).find(".track-title"));
+	function selectAlbum(element, curr_element) {
+		console.log("chosen is: " + chosen_album);
+		if (chosen_album != "none") {
+			chosen_album.toggle();
+		}
+		chosen_album = element;
+		chosen_album.toggle();
+	}
+
+	function getIframe(url, element) {
+		console.log("getting iframe for: " + url);
+		SC.oEmbed(url, {
+			auto_play : false,
+			maxwidth : 640,
+			color : "660066",
+		}, function(oembed) {
+			console.log('embed return: ' + oembed.title)
+			console.log('embed return: ' + oembed.html)
+			element.html(oembed.html);
 		});
-	});
-	$(".album-button").click(function() {
-		var row = "album-" + $(this).attr("id");
-		var element = $(document).find("#"+row);
-		element.toggle();
-	});
-	$(".track").click(function(e) {
-		var url = $(this).find(".track-url").html();
-		getIframe(url, $(this).find(".track-title"));
-	})
-});
+	}
+
+	$(document).ready(
+			function() {
+				$(".album-table").each(
+						function() {
+							$(this).find(".track").each(
+									function() {
+										getIframe($(this).find(".track-url")
+												.html(), $(this).find(
+												".track-title"));
+									});
+						});
+				$(".album-button").click(function() {
+					var row = "album-" + $(this).attr("id");
+					var element = $(document).find("#" + row);
+					selectAlbum(element);
+				});
+				$(".track").click(function(e) {
+					var url = $(this).find(".track-url").html();
+					getIframe(url, $(this).find(".track-title"));
+				})
+			});
+};
+
+scope();
